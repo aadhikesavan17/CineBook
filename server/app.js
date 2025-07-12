74a838d3151b5659956055f114ff66938bbb458e
Aadhikesavan <aadhikesavan17u@gmail.com>
2025-07-12 18:30:38 +0530

    Use env variables for MySQL connection

--- server/app.js
+++ server/app.js
@@ -9,13 +9,22 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MySQL
+/*// Connect to MySQL
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Aadhi@17",
   database: "movieapp"
+}); */
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
+
 
 db.connect(err => {
   if (err) throw err;
